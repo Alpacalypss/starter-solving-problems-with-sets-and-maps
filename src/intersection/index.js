b59875886b3d0b74099b291f083b40279baa6f4e
Cain Bailey <cainbailey1993@gmail.com>
2024-01-29 23:43:10 -0800

    Solved algorithms

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,22 @@
-function intersection(a, b) {}
+/*initialize a new Map
+initialize a new Set
+for each element e of a do:
+  add e to the Map
+
+for each element e of b do:
+  lookup e in the Map
+  if e is in the Map then
+    add e to the Set
+convert the Set to an array and return*/
+
+function intersection(a, b) {
+  let map = new Map();
+  let set = new Set();
+  a.forEach((e) => map.set(e));
+  b.forEach((e) => {
+    if (map.has(e)) set.add(e);
+  });
+  return [...set];
+}
 
 module.exports = intersection;
--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -1,7 +1,37 @@
 /**
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
+ * Input: an array of words named words
+
+instantiate a new Map named results
+
+for each word in the words array do:
+  lowercase word
+  find all unique letters in word
+    create a Set with all the letters of the word to remove duplicates
+    create an array of letters from the Set and sort alphabetically
+    use join to create a string from the array
+  if the sorted string of letters is a key in the results Map then
+    add word to the array associated with that key
+  else
+    make a new entry in the results Map with key = sorted string of letters and the value an array with word as its only element
+return the results Map
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  let results = new Map();
+
+  words.forEach((word) => {
+    word.toLowerCase();
+    let uniqueLetters = Array.from(new Set(word.split("")))
+      .sort()
+      .join("");
+    if (results.has(uniqueLetters)) {
+      results.get(uniqueLetters).push(word);
+    } else {
+      results.set(uniqueLetters, [word]);
+    }
+  });
+  return results;
+}
 
 module.exports = sameLetters;
